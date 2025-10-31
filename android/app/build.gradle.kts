8c5a71375b8255612db9929dd0c8379c53dcd52e
basitmurad <basitmurad7@gmail.com>
2025-10-31 12:48:14 +0500

    remove the cookies section and updated the version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         applicationId = "com.benjamin_pro.afronika_app"
         minSdk = 24       // ✅ Updated (was 23)
         targetSdk = 36    // ✅ Updated (was 35)
-        versionCode = 9
-        versionName = "1.0.9"
+        versionCode = 10
+        versionName = "1.0.10"
     }
 
     signingConfigs {
